556d5323d883ef224c1b706a9ecfc1bb9bea2604
Brais Garrido Blanco <braisgb@gmail.com>
2025-02-26 12:18:11 +0100

    Correcion gradle

--- plugin/src/test/java/runner/TestRunner.java
+++ plugin/src/test/java/runner/TestRunner.java
@@ -1,5 +1,12 @@
 package runner;
 
+import org.junit.AfterClass;
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import pages.BasePage;
+
 public class TestRunner {
     
 }
