c99b39134a40faa27a12135003d6997cd02fd4fb
BraisGarrido <braisgb@gmail.com>
2025-02-26 13:23:27 +0100

    Correccion estructura

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,12 +1,10 @@
 package runner;
 
-import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import pages.BasePage;
 
+@RunWith(Cucumber.class)
 public class TestRunner {
     
 }
